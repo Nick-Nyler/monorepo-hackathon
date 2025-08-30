c59ec0f4616895d825c7c6e6d7241396256719e9
Nick <nicknyler@gmail.com>
2025-08-30 15:38:36 +0300

    completed stlye

--- packages/web-dashboard/src/App.tsx
+++ packages/web-dashboard/src/App.tsx
@@ -9,9 +9,9 @@ import './App.css'
 function App() {
   return (
     <Router>
-      <div className="min-h-screen bg-gray-50">
+      <div className="min-h-screen bg-slate-50">
         <Navigation />
-        <main className="container mx-auto px-4 py-8">
+        <main className="container p-8">
           <Routes>
             <Route path="/" element={<Dashboard />} />
             <Route path="/analysis" element={<Analysis />} />
